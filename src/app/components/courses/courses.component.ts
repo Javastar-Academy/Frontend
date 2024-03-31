ca156e92ae8fc1aeef4e2ddad24ec813a893cce6
Teodorvca <teodorvoica2@gmail.com>
2024-03-31 12:25:30 +0300

    Modify the app so you can subscribe to newsletter and register a new student. Added a registration page for studend. I need the IPs for the backend

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CourseService} from "../../services/course.service";
+import {Course} from "../../common/course";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-courses',
@@ -8,8 +11,21 @@ import { Component } from '@angular/core';
 export class CoursesComponent {
 
 
-  constructor() {
+  constructor(private courseService : CourseService) {
+    this.getCourseList();
   }
 
+  getCourseList(): void{
+    this.courseService.getCourseList().subscribe(
+        {
+          next: (res: Course[]) => {
+            console.log(res);
+          },
+          error: (err: HttpErrorResponse) => {
+            console.log(err);
+          }
+        }
+    );
+  }
 
 }
---
+++ src/app/components/register/register.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RegisterComponent } from './register.component';
+import { RegisterService } from '../../services/register.service';
+
+describe('RegisterComponent', () => {
+    let component: RegisterComponent;
+    let fixture: ComponentFixture<RegisterComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FormsModule, HttpClientTestingModule],
+            declarations: [ RegisterComponent ],
+            providers: [ RegisterService ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    // Additional tests can be added here
+});
